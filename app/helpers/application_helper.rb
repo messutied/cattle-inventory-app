62e2382304865aff7a2c254403cf582c0c1f8e02
unknown <messuti.edd@.(none)>
2012-04-30 17:43:24 -0400

    twitter bootstrap, nav menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,30 @@ module ApplicationHelper
   def link_to_remove_fields(name, f)
     f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
   end
+
+  def nav_sel(page)
+    case page
+    when "home"
+      if params[:controller] == "home" 
+        return raw(" class=\"active\"") 
+      end
+    when "in_eg_new"
+      if params[:controller] == "movimientos" and params[:action] == "new" and params[:type] == "in_eg"
+        return raw(" class=\"active\"")
+      end
+    when "mov_new"
+      if params[:controller] == "movimientos" and params[:action] == "new" and params[:type] == "mov"
+        return raw(" class=\"active\"")
+      end
+    when "in_eg_list"
+      if params[:controller] == "movimientos" and params[:action] == "index" and params[:type] == "in_eg"
+        return raw(" class=\"active\"")
+      end
+    when "mov_list"
+      if params[:controller] == "movimientos" and params[:action] == "index" and params[:type] == "mov"
+        return raw(" class=\"active\"")
+      end
+    end
+
+  end
 end
