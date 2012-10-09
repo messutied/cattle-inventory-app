be5284a516bacd235d09af93587c45bc2b57d32b
Eduardo Messuti <messuti.edd@gmail.com>
2012-10-09 10:13:09 -0400

    cookie fix(?)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Ganaderia::Application.config.session_store :cookie_store, :key => '_Ganaderia_session'
+Ganaderia::Application.config.session_store :cookie_store, :key => '_Ganaderia_session2'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
