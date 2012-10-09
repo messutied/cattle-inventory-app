e072b1b4cbdb653e2190c3a863e29826dead88cf
Eduardo Messuti <messuti.edd@gmail.com>
2012-10-09 10:39:12 -0400

    fix assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,8 @@ Ganaderia::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  config.assets.precompile += %w( bootstrap/bootstrap.css )
+  config.assets.precompile += %w( bootstrap/bootstrap-responsive.css )
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
