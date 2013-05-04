e6f58d85ea3730b7568f9ad5aab34cb6484b2f3a
Eduardo Messuti <messuti.edd@gmail.com>
2013-05-04 00:44:14 -0400

    fix menu show/hide

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -2,19 +2,19 @@
 (function($) {
 
     function menuHide() {
-        $(".span2").hide();
+        $(".span3").hide();
         $("#hide-menu i").removeClass("icon-minus-sign").addClass("icon-plus-sign");
         $("#hide-menu span").html("Mostrar Menú");
-        $(".span10").css("margin-left", "0");
-        $(".span10").removeClass("span10").addClass("span12");
+        $(".span9").css("margin-left", "0");
+        $(".span9").removeClass("span9").addClass("span12");
     }
 
     function menuShow() {
-        $(".span2").show();
+        $(".span3").show();
         $("#hide-menu i").removeClass("icon-plus-sign").addClass("icon-minus-sign");
         $("#hide-menu span").html("Esconder Menú");
-        $(".span12").removeClass("span12").addClass("span10");
-        $(".span10").css("margin-left", "30px");
+        $(".span12").removeClass("span12").addClass("span9");
+        $(".span9").css("margin-left", "30px");
     }
 
     $(document).ready(function() {
@@ -27,21 +27,21 @@
 
         // Hide/Show Menu
         $("#hide-menu").click(function() {
-            if ($(".span2").is(":visible")) { // HIDE THE MENU
-                $(".span2").hide();
+            if ($(".span3").is(":visible")) { // HIDE THE MENU
+                $(".span3").hide();
                 $("#hide-menu i").removeClass("icon-minus-sign").addClass("icon-plus-sign");
                 $("#hide-menu span").html("Mostrar Menú");
-                $(".span10").css("margin-left", "0");
-                $(".span10").removeClass("span10").addClass("span12");
+                $(".span9").css("margin-left", "0");
+                $(".span9").removeClass("span9").addClass("span12");
 
                 $.cookie("menu_state", "hide");
             }
             else { // SHOW THE MENU
-                $(".span2").show();
+                $(".span3").show();
                 $("#hide-menu i").removeClass("icon-plus-sign").addClass("icon-minus-sign");
                 $("#hide-menu span").html("Esconder Menú");
-                $(".span12").removeClass("span12").addClass("span10");
-                $(".span10").css("margin-left", "30px");
+                $(".span12").removeClass("span12").addClass("span9");
+                $(".span9").css("margin-left", "30px");
 
                 $.cookie("menu_state", "show");
             }
